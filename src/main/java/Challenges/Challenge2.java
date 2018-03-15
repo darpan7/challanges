4e138626ceb8f105b2abf684e156c7a2d44bd579
Darpan Shah <dashah@dev.terastrm.net>
2018-03-15 13:59:13 -0400

    Working Solution!

--- src/main/java/Challenges/Challenge2.java
+++ src/main/java/Challenges/Challenge2.java
@@ -12,19 +12,25 @@ import java.util.Scanner;
 Problem:
 =======
 Given a positive integer n, count the total number of set bits in binary representation of all numbers from 1 to n.
-
 Examples:
 Input: n = 3
 Output:  4
-
 Input: n = 6
 Output: 9
-
 Input: n = 7
 Output: 12
-
 Input: n = 8
 Output: 13
+
+Program Input: No of test cases, then following number for each test case
+3
+15
+17
+3
+Program Output:
+32
+35
+4
  */
 public class Challenge2 {
 	public static void main(String[] args) {
@@ -34,43 +40,54 @@ public class Challenge2 {
 		while(T > 0){
 			scanner.nextLine();
 			int n = scanner.nextInt();
-			p(Integer.toBinaryString(n));
-			String binary = Integer.toBinaryString(n);
-			int bits = binary.length();
-			//p("bits " + bits);
-			int check = (int) (Math.pow(2, bits)-1);
-			if(check == n){
-				p(bits*Math.pow(2, bits-1));
-			}else{
-				//double total1s = bits*Math.pow(2, bits-1);
-				double bottomline = (bits-1)*Math.pow(2, bits-2);
-				//p("bottom line: " + bottomline);
-				int prev = (int) (Math.pow(2, bits-1)-1);
-				//p("prev " + prev);
-				int diff = n - prev;
-				double total1s = bottomline;
-				//p("diff " + diff);
-				while(diff>1){
-					bits = Integer.toBinaryString(diff).length();
-					check = (int) (Math.pow(2, bits)-1);
-					if(check == diff){
-						total1s += (bits*Math.pow(2, bits-1));
-						break;
+			int bits = bitsInBinary(n);
+			int check = (int) (Math.pow(2, bits) - 1); // 2^bits -1
+			boolean isPowerOf2 = isPowerOf2s(check, n, bits);
+			if(!isPowerOf2){
+				double setBitsInClosest2sPower = (bits - 1) * Math.pow(2, bits - 2); // (bits-1)*2^bits-2
+				int previousNumber = (int) (Math.pow(2, bits - 1) - 1); // 2^bits-1 - 1
+				int difference = n - previousNumber;
+				long total1s = (long) setBitsInClosest2sPower; // minimum set bits.
+				boolean isSetBit = false;
+				// find no of set bits in difference.
+				for (int i = 0; i < bits; i++) {
+					int power = (int) Math.pow(2, i);
+					int divide = (difference / power);
+					int modulo = difference % power;
+					if (divide > 0) {
+						isSetBit = true;
 					}
-					bottomline = (bits-1)*Math.pow(2, bits-2);
-					prev = (int) (Math.pow(2, bits-1)-1);
-					diff = diff - prev;
-					total1s += bottomline;
+					if (i == (bits - 1)) {
+						total1s += modulo;
+					} else if (i == 0) {
+						total1s += (difference / 2);
+					} else if (isSetBit) {
+						int decide = (difference / power);
+						int times = (decide / 2) * power;
+						int onesForThis2sPower = times + ((decide % 2 == 0) ? 0 : modulo);
+						total1s += onesForThis2sPower;
+					}
+
+					isSetBit = false;
 				}
-				p((total1s+1));
+				p(total1s);
 			}
 			--T;
 		}
-		
-		
 		scanner.close();
 	}
+	public static boolean isPowerOf2s(int check, int n, int bits){
+		if(check == n){
+			p((long) (bits*Math.pow(2, bits-1)));
+			return true;
+		}
+		return false;
+	}
 	
+	public static int bitsInBinary(int n){
+		String binary = Integer.toBinaryString(n);
+		return binary.length();
+	}
 	public static void p(Object o) {
 		System.out.println(o.toString());
 	}
