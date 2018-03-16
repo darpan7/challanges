e6d657196161afce37a3e45eaff11ba410e3cf27
Darpan Shah <dashah@dev.terastrm.net>
2018-03-16 02:01:09 -0400

    challenge3: Working Solution [Though not optimized]
    - Need to convert into O(n) form.

--- src/main/java/Challenges/Challenge3.java
+++ src/main/java/Challenges/Challenge3.java
@@ -1,9 +1,12 @@
 package Challenges;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
 * @author {
@@ -48,16 +51,19 @@ public class Challenge3 {
 		scanner.nextLine();
 		while(T > 0){
 			String string = scanner.nextLine();
-			Graph graph = new Graph<Node>();
+			Graph<Node> graph = new Graph<Node>();
 			boolean rw = false;
 			for(int i=0; i<string.length(); i++){
 				String name = String.valueOf(string.charAt(i));
 				if("c".equals(name)){
+					rw = true;
+				}else{
 					rw = false;
 				}
 				graph.addNode(new Node(name, i, rw));
 			}
-			
+			graph.calculate();
+			//System.out.println(graph.count);
 			--T;
 		}
 		scanner.close();
@@ -66,26 +72,136 @@ public class Challenge3 {
 		String name;
 		int index;
 		List<Node> inbound;
-		List<Node> outbound;
 		boolean isTerminal;
 		
 		public Node(String name, int index, boolean isTerminal){
 			this.name = name;
 			this.index = index;
 			this.isTerminal = isTerminal;
+			this.inbound = new ArrayList<Node>();
 		}
+
+		@Override
+		public String toString() {
+			return "Node [" + name + ":" + index + "]";
+		}
+		
 		
 	}
 	public static class Graph<V extends Node> {
-		Map<String, V> nodes;
+		Map<String, List<V>> nodes;
+		List<V> nodesByIndex;
+		List<V> startingNodes;
+		List<Integer> As;
+		List<Integer> Bs;
+		List<Integer> Cs;
+		int count;
 		
 		public Graph(){
-			nodes = new HashMap<String, V>();
+			nodes = new HashMap<String, List<V>>();
+			startingNodes = new ArrayList<V>();
+			initiate();
 		}
-		
+		public void initiate(){
+			nodesByIndex = new ArrayList<V>();
+			As = new ArrayList<Integer>();
+			Bs = new ArrayList<Integer>();
+			Cs = new ArrayList<Integer>();
+		}
+		public void sort(){
+			Collections.sort(As);
+			Collections.sort(Bs);
+			Collections.sort(Cs);
+		}
 		public void addNode(V node){
-			nodes.put(node.name, node);
+			if(node.isTerminal){
+				//System.out.println("Adding null for " + node.name + node.index);
+				node.inbound.add(null);
+			}
+			List<V> typedNodes = nodes.get(node.name);
+			nodesByIndex.add(node);
+			if(typedNodes==null){
+				typedNodes = new ArrayList<V>();
+			}
+			typedNodes.add(node);
+			nodes.put(node.name, typedNodes);
+			switch(node.name){
+			case "a":
+				startingNodes.add(node);
+				for(int i=0; i<node.index; i++){
+					V prev = nodesByIndex.get(i);
+					if(prev.name.equals(node.name)){
+						prev.inbound.add(node);
+					}
+				}
+				break;
+			case "b":
+				//Bs.add(node.index);
+				for(int i=0; i<node.index; i++){
+					V prev = nodesByIndex.get(i);
+					if(prev.name.equals(node.name) || prev.name.equals("a")){
+						prev.inbound.add(node);
+					}
+				}
+				break;
+			case "c":
+				//Cs.add(node.index);
+				for(int i=0; i<node.index; i++){
+					V prev = nodesByIndex.get(i);
+					if(prev.name.equals(node.name) || prev.name.equals("b")){
+						prev.inbound.add(node);
+					}
+				}
+				break;
+			default:
+			}
+			//calculate();
 		}
+		public void rec(List<Node> start, String s, boolean end){
+			//System.out.println(start);
+			for(Node n: start){
+				if(end && n == null){
+					System.out.println(s);
+					continue;
+				}
+				//System.out.print("\t" + n.name + n.index + ": ");
+				rec(n.inbound, s+n.name+n.index, n.isTerminal);
+			}
+			
+		}
+		public void calculate(){
+			for(V start: startingNodes){
+				//System.out.print(start.name + start.index + ": " + start.inbound + "\n");
+				rec(start.inbound, start.name+start.index, start.isTerminal);
+			}
+			////////
+//			sort();
+//			Set<String> set = nodes.keySet();
+//			for(String type: set){
+//				List<V> list = nodes.get(type);
+//				switch(type){
+//				case "a":
+//					for(V node: list){
+//						int id = As.indexOf(node.index);
+//						if(id < 0){
+//							
+//						}else if (id == As.size()-1){
+//							
+//						}else{
+//							for(int i=id+1; i<As.size(); i++){
+//								node.inbound.add(node);
+//							}
+//						}
+//					}
+//					break;
+//				case "b":
+//					break;
+//				case "c":
+//					break;
+//				default:
+//				}
+//			}
+		} 
 		
 	}
 }
